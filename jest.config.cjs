36d437d4c51435d84826231bfc3b79fe063444c4
Zalenski Egor <egor.zalenski@softeq.com>
2024-10-25 08:40:47 +0200

    Update jest.config.cjs

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,7 +1,7 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   testEnvironmentOptions: {
-    url: 'http://localhost/'
+    url: 'http://localhost'
   },
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|ico|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
